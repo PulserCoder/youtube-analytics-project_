d87a5219e15c94c97df1c88934e8eb2d96290320
PulserCoder <92484659+PulserCoder@users.noreply.github.com>
2023-09-28 19:57:03 +0300

    done 2

--- src/channel.py
+++ src/channel.py
@@ -18,13 +18,36 @@ class Channel:
         self.title = self.channel_info['items'][0]['snippet']['title']
         self.description = self.channel_info['items'][0]['snippet']['description']
         self.url = self.channel_info['items'][0]['snippet']['customUrl']
-        self.subscriber_count = self.channel_info['items'][0]['statistics']['subscriberCount']
+        self.subscriber_count = int(self.channel_info['items'][0]['statistics']['subscriberCount'])
         self.video_count = self.channel_info['items'][0]['statistics']['videoCount']
         self.view_count = self.channel_info['items'][0]['statistics']['viewCount']
 
     def print_info(self) -> None:
         """Выводит в консоль информацию о канале."""
         print(json.dumps(self.channel_info, indent=2, ensure_ascii=False))
+
+    def __str__(self):
+        return f"{self.title}({self.url})"
+
+    def __add__(self, other):
+        self.subscriber_count = self.subscriber_count + other.subscriber_count
+        return self.subscriber_count
+
+    def __sub__(self, other):
+        self.subscriber_count = self.subscriber_count - other.subscriber_count
+        return self.subscriber_count
+
+    def __eq__(self, other):
+        return self.subscriber_count == other.subscriber_count
+
+    def __ne__(self, other):
+        return self.subscriber_count != other.subscriber_count
+
+    def __lt__(self, other):
+        return self.subscriber_count < other.subscriber_count
+
+    def __le__(self, other):
+        return self.subscriber_count <= other.subscriber_count
 
     @classmethod
     def get_service(cls) -> Resource:
