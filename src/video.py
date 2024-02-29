e04cdfc168bc6349581de1f016bdeb87f6af6ac3
PulserCoder <92484659+PulserCoder@users.noreply.github.com>
2024-02-29 15:43:44 +0300

    done 6

--- src/video.py
+++ src/video.py
@@ -10,15 +10,19 @@ class Video:
     youtube = build('youtube', 'v3', developerKey=API_KEY)
 
     def __init__(self, video_id: str):
-        self.video_id = video_id
-        self._video = Video.youtube.videos().list(
-            part="snippet,contentDetails,statistics",
-            id=video_id
-        ).execute()
-        self.title = self._video['items'][0]['snippet']['title']
-        self.url = self._video['items'][0]['snippet']['thumbnails']['default']['url']
-        self.view_count = self._video['items'][0]['statistics']['viewCount']
-        self.likes_count = self._video['items'][0]['statistics']['likeCount']
+        try:
+            self.video_id = video_id
+            self._video = Video.youtube.videos().list(part='snippet,statistics', id=video_id).execute()
+            self.title = self._video['items'][0]['snippet']['title']
+            self.url = self._video['items'][0]['snippet']['thumbnails']['default']['url']
+            self.view_count = self._video['items'][0]['statistics']['viewCount']
+            self.likes_count = self._video['items'][0]['statistics']['likeCount']
+        except IndexError:
+            self._video = None
+            self.title = None
+            self.url = None
+            self.view_count = None
+            self.likes_count = None
 
     def __str__(self):
         return self.title
